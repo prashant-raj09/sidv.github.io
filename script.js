587b48e055ecc4c43fa2891c0949c402341d63ce
prashant-raj09 <95146202+prashant-raj09@users.noreply.github.com>
2022-11-21 23:06:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// ============ typing animation=========
+var typed = new Typed(".typing",{
+    strings:["","RTL Designer","Design Verification Engineer","Physical Designer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
